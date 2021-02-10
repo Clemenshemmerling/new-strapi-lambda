be47ea4139ea27e4dbcbb1980d9c3fd39aef7136
Clemens Hemmerling <clemens@getkor.io>
2021-02-09 19:35:55 -0600

    test strapi build

---
+++ app.js
@@ -0,0 +1,17 @@
+
+const startStrapi = require("strapi");
+
+const serverless = require("serverless-http");
+
+const strapiInstance = startStrapi();
+
+let strapiStartPromise = null;
+
+module.exports.handler = async (event, context) => {
+  if (strapiStartPromise === null) {
+    strapiStartPromise = strapiInstance.start();
+  }
+  await strapiStartPromise;
+  const handler = serverless(strapiInstance.app);
+  return handler(event, context);
+};
